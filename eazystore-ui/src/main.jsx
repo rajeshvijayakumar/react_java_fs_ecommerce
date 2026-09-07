717fc9b8da1d31d16522bb72e90fda4f97724140
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-07 17:35:31 +0530

    [ReactFrontend]: Route definition created using Route component from route dom library and replaced the old mechanism for ui navigation routes..

--- eazystore-ui/src/main.jsx
+++ eazystore-ui/src/main.jsx
@@ -2,49 +2,74 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Contact from "./components/Contact.jsx";
 import Cart from "./components/Cart.jsx";
 import Login from "./components/Login.jsx";
 import About from "./components/About.jsx";
-import Home from './components/Home.jsx';
+import Home from "./components/Home.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
 
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
+// This method of routes mechanism used commonly and complex applications and have better readability
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+  </Route>,
+);
 
-      {
-        path: "/about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+const appRouter = createBrowserRouter(routeDefinitions);
+
+
+
+
+// This method routes used in the simple and small applications
+// const appRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "/home",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+
+//       {
+//         path: "/cart",
+//         element: <Cart />,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
+
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={appRouter} />
