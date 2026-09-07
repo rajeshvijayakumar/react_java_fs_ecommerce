09d8cc8d064fb5352ef8b1ea9c6f8752edae7819
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-07 16:29:39 +0530

    [ReactFrontend]: Errorpage created for invalid path access and integrated with ui navigation routes..

--- eazystore-ui/src/main.jsx
+++ eazystore-ui/src/main.jsx
@@ -8,11 +8,13 @@ import Cart from "./components/Cart.jsx";
 import Login from "./components/Login.jsx";
 import About from "./components/About.jsx";
 import Home from './components/Home.jsx';
+import ErrorPage from "./components/ErrorPage.jsx";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
