30c0de9b9ac587c46ceb9cb3cfc15a91bc79f08d
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-10 20:29:43 +0530

    [ReactFE]: Cart functionality replaced with react redux with react context across the app..

--- eazystore-ui/src/store/cart-context.jsx
+++ eazystore-ui/src/store/cart-context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 // // STEP 2
 // export const initialCartContext = {
 //   cart: [],
@@ -15,12 +15,58 @@ import { useEffect, useState } from "react";
 // STEP 1
 export const CartContext = createContext();
 
-export const useCart = () =>  useContext(CartContext);
+export const useCart = () => useContext(CartContext);
+
+const ADD_TO_CART = "ADD_TO_CART";
+const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+const CLEAR_CART = "CLEAR_CART";
+
+const cartReducer = (prevCart, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const { product, quantity } = action.payload;
+
+      const existingItem = prevCart.find(
+        (item) => item.productId === product.productId,
+      );
+
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.productId === product.productId
+            ? { ...item, quantity: item.quantity + quantity }
+            : item,
+        );
+      }
+
+      return [...prevCart, { ...product, quantity }];
+
+    case REMOVE_FROM_CART:
+      return prevCart.filter(
+        (item) => item.productId !== action.payload.productId,
+      );
+    case CLEAR_CART:
+      return [];
+    default:
+      return prevCart;
+  }
+};
 
 // STEP 3
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
   // Initialize cart state from localStorage or as an empty array
-  const [cart, setCart] = useState(() => {
+  // Moving to reducer initial state
+  // const [cart, setCart] = useState(() => {
+  //   try {
+  //     const storedCart = localStorage.getItem("cart");
+  //     return storedCart ? JSON.parse(storedCart) : [];
+  //   } catch (error) {
+  //     console.error("Failed to parse cart from localStorage:", error);
+  //     return [];
+  //   }
+  // });
+
+  //initial cart state for reducer
+  const initialCartState = (() => {
     try {
       const storedCart = localStorage.getItem("cart");
       return storedCart ? JSON.parse(storedCart) : [];
@@ -28,7 +74,10 @@ export const CartProvider = ({children}) => {
       console.error("Failed to parse cart from localStorage:", error);
       return [];
     }
-  });
+  })();
+
+  // Iniitiaze reducer with initial cart state below
+  const [cart, dispatch] = useReducer(cartReducer, initialCartState);
 
   // Save cart to localStorage whenever it changes
   useEffect(() => {
@@ -40,30 +89,39 @@ export const CartProvider = ({children}) => {
   }, [cart]);
 
   const addToCart = (product, quantity) => {
-    setCart((prevCart) => {
-      const existingItem = prevCart.find(
-        (item) => item.productId === product.productId,
-      );
+    // Moving this add to cart logic to reducer
+    // setCart((prevCart) => {
+    //   const existingItem = prevCart.find(
+    //     (item) => item.productId === product.productId,
+    //   );
 
-      if (existingItem) {
-        // Use map() to create a new array with updated quantity
-        return prevCart.map((item) =>
-          item.productId === product.productId
-            ? { ...item, quantity: item.quantity + quantity }
-            : item,
-        );
-      }
+    //   if (existingItem) {
+    //     // Use map() to create a new array with updated quantity
+    //     return prevCart.map((item) =>
+    //       item.productId === product.productId
+    //         ? { ...item, quantity: item.quantity + quantity }
+    //         : item,
+    //     );
+    //   }
 
-      // If the product is not in the cart, add it
-      return [...prevCart, { ...product, quantity }];
-    });
+    //   // If the product is not in the cart, add it
+    //   return [...prevCart, { ...product, quantity }];
+    // });
+
+    dispatch({ type: ADD_TO_CART, payload: { product, quantity } });
   };
 
   // // Function to remove an item from the cart
   const removeFromCart = (productId) => {
-    setCart((prevCart) =>
-      prevCart.filter((item) => item.productId !== productId),
-    );
+    // setCart((prevCart) =>
+    //   prevCart.filter((item) => item.productId !== productId),
+    // );
+    dispatch({ type: REMOVE_FROM_CART, payload: { productId } });
+  };
+
+  // Function to clear all the items in the cart
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
   };
 
   // Calculate total quantity
@@ -71,9 +129,15 @@ export const CartProvider = ({children}) => {
 
   return (
     <CartContext.Provider
-      value={{ cart, setCart, addToCart, removeFromCart, totalQuantity }}
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        totalQuantity,
+      }}
     >
       {children}
     </CartContext.Provider>
   );
-};;
+};
