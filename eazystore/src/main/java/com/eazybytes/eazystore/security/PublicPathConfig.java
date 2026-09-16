48299aa9b323fcd7f725bd2d25a7d3eee87b9e60
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-16 15:07:45 +0530

    [SpringBoot]:Authorization Fix: When roles doesn't match spring framework will throw error 403 but it is throwing 401 due to the error path is not configured as public path..

--- eazystore/src/main/java/com/eazybytes/eazystore/security/PublicPathConfig.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/security/PublicPathConfig.java
@@ -14,7 +14,8 @@ public class PublicPathConfig {
         return List.of(
                 "/api/v1/products/**",
                 "/api/v1/contacts/**",
-                "/api/v1/auth/**"
+                "/api/v1/auth/**",
+                "/error"
         );
     }
 }
