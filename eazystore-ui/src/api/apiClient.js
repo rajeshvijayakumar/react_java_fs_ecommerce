3799621cb21768440b267d41df413e44d571bd12
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-16 19:15:01 +0530

    [ReactFE]: csrf token api integrated and handled the cookie w.r.to it..

--- eazystore-ui/src/api/apiClient.js
+++ eazystore-ui/src/api/apiClient.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    "Accept":"application/json"
+    Accept: "application/json",
   },
-  timeout: 10000
+  timeout: 10000,
+  withCredentials: true,
 });
 
 apiClient.interceptors.request.use(
@@ -14,6 +16,25 @@ apiClient.interceptors.request.use(
     if (jwtToken) {
       config.headers.Authorization = `Bearer ${jwtToken}`;
     }
+
+    // Only fetch CSRF token for non-safe methods
+    const safeMethods = ["GET", "HEAD", "OPTIONS"];
+    if (!safeMethods.includes(config.method.toUpperCase())) {
+       let csrfToken = Cookies.get("XSRF-TOKEN");
+
+        if (!csrfToken) {
+          await axios.get(`${import.meta.env.VITE_API_BASE_URL}/csrf-token`, {
+            withCredentials: true,
+          });
+
+           csrfToken = Cookies.get("XSRF-TOKEN");
+           if (!csrfToken) {
+             throw new Error("Failed to retrieve CSRF token from cookies");
+           }
+        }
+         config.headers["X-XSRF-TOKEN"] = csrfToken;
+    }
+
     return config;
   },
   (error) => Promise.reject(error),
