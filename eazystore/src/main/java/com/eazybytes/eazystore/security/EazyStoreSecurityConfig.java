d9f08c2994cdbb48ddfebf18b0892b0d58a6d329
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-16 19:14:20 +0530

    [SpringBoot]: Security: Csrf token api created and configured successfully.

--- eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -39,7 +41,10 @@ public class EazyStoreSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
             throws Exception {
-        return http.csrf(csrfConfig -> csrfConfig.disable())
+        return http.csrf(csrfConfig -> csrfConfig.
+                            csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+
                 .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests((requests) -> {
                             publicPaths.forEach(path ->
--- eazystore/src/main/java/com/eazybytes/eazystore/security/PublicPathConfig.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/security/PublicPathConfig.java
@@ -15,7 +15,8 @@ public class PublicPathConfig {
                 "/api/v1/products/**",
                 "/api/v1/contacts/**",
                 "/api/v1/auth/**",
-                "/error"
+                "/error",
+                "/api/v1/csrf-token"
         );
     }
 }
