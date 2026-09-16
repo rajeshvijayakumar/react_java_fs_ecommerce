8bc7b2704a8d9dab153cadd86d51d19b9170b381
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-16 12:05:42 +0530

    [SpringBoot]: Added user based auditing for DB...

--- eazystore/src/main/java/com/eazybytes/eazystore/config/AuditAwareImpl.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/config/AuditAwareImpl.java
@@ -1,6 +1,9 @@
 package com.eazybytes.eazystore.config;
 
+import com.eazybytes.eazystore.entity.Customer;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -10,6 +13,24 @@ public class AuditAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("Anonymous user");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || !authentication.isAuthenticated() ||
+                authentication.getPrincipal().equals("anonymousUser")) {
+
+            return Optional.of("Anonymous user");
+        }
+
+        Object principal = authentication.getPrincipal();
+        String userName;
+
+        if(principal instanceof Customer customer){
+            userName = customer.getEmail();
+        } else {
+            userName = principal.toString(); // fallback mechanism
+        }
+
+        return Optional.of(userName);
     }
 }
