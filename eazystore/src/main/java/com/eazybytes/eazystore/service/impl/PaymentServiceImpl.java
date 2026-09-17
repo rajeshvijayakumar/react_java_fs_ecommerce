1771da6643128ef2c7a435da5613f46f58fbb067
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-17 17:11:54 +0530

    [SpringBoot]: Implemented and created payment api for stripe payment service in the project.

---
+++ eazystore/src/main/java/com/eazybytes/eazystore/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,33 @@
+package com.eazybytes.eazystore.service.impl;
+
+import com.eazybytes.eazystore.dto.PaymentIntentRequestDto;
+import com.eazybytes.eazystore.dto.PaymentIntentResponseDto;
+import com.eazybytes.eazystore.service.IPaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements IPaymentService {
+
+    @Override
+    public PaymentIntentResponseDto createPaymentIntent(PaymentIntentRequestDto requestDto) {
+        try {
+
+            PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+                    .setAmount(requestDto.amount())
+                    .setCurrency(requestDto.currency())
+                    .addPaymentMethodType("card").build();
+
+            PaymentIntent paymentIntent = PaymentIntent.create(params);
+
+            return new PaymentIntentResponseDto(paymentIntent.getClientSecret());
+
+        } catch (StripeException exception) {
+            throw new RuntimeException("Failed to create payment intent ", exception);
+        }
+    }
+}
