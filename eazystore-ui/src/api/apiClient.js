d850655fe6f8d47854a99bb20a8086249918332f
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-18 14:14:37 +0530

    [ReactFE]: If Jwt token expired, in the frontend side for better handling we added interceptors to redirect the user to the login page..

--- eazystore-ui/src/api/apiClient.js
+++ eazystore-ui/src/api/apiClient.js
@@ -40,4 +40,19 @@ apiClient.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
+
+apiClient.interceptors.response.use(
+   (response) => response,
+   async(error) => {
+        if(error.response && error.response.status === 401){
+          const jwtToken = localStorage.getItem("jwtToken");
+          if(jwtToken) {
+                localStorage.removeItem("jwtToken");
+                window.location.href = "/login";
+          }
+        }
+        return Promise.reject(error);
+   }
+);
+
 export default apiClient;
