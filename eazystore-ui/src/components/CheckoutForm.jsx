6c9773eefbb00f457bc6d924fbe490a6107fcaf3
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-21 11:56:37 +0530

    [ReactFE]: Integrated the orders api for the organic customers..

--- eazystore-ui/src/components/CheckoutForm.jsx
+++ eazystore-ui/src/components/CheckoutForm.jsx
@@ -112,25 +112,24 @@ export default function CheckoutForm() {
         setErrorMessage(error.message || "Payment failed. Please try again.");
       } else if (paymentIntent && paymentIntent.status === "succeeded") {
         toast.success("Payment successful!");
-        navigate("/order-success");
-    //     try {
-    //       await apiClient.post("/orders", {
-    //         totalPrice: totalPrice,
-    //         paymentId: paymentIntent.id,
-    //         paymentStatus: paymentIntent.status,
-    //         items: cart.map((item) => ({
-    //           productId: item.productId,
-    //           quantity: item.quantity,
-    //           price: item.price,
-    //         })),
-    //       });
-    //       sessionStorage.setItem("skipRedirectPath", "true");
-    //       clearCart();
-    //       navigate("/order-success");
-    //     } catch (orderError) {
-    //       console.error("Failed to create order:", orderError);
-    //       setErrorMessage("Order creation failed. Please contact support.");
-    //     }
+        try {
+          await apiClient.post("/orders", {
+            totalPrice: totalPrice,
+            paymentId: paymentIntent.id,
+            paymentStatus: paymentIntent.status,
+            items: cart.map((item) => ({
+              productId: item.productId,
+              quantity: item.quantity,
+              price: item.price,
+            })),
+          });
+          sessionStorage.setItem("skipRedirectPath", "true");
+          clearCart();
+          navigate("/order-success");
+        } catch (orderError) {
+          console.error("Failed to create order:", orderError);
+          setErrorMessage("Order creation failed. Please contact support.");
+        }
       }
     } catch (error) {
       setErrorMessage("Error processing payment. Please try again later.");
