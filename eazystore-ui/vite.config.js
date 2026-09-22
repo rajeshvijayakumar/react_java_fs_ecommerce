5b46e8301fb37e3e3cc483e91165d27e86342f6b
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-22 18:02:15 +0530

    [ReactFE]:[Build&Deployment]: Added default,dev and production environment to react app and configured...

--- eazystore-ui/vite.config.js
+++ eazystore-ui/vite.config.js
@@ -1,8 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss  from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "vendor",
+              test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            },
+            {
+              name: "redux",
+              test: /[\\/]node_modules[\\/](@reduxjs\/toolkit|react-redux)[\\/]/,
+            },
+            {
+              name: "router",
+              test: /[\\/]node_modules[\\/](react-router-dom)[\\/]/,
+            },
+            {
+              name: "ui",
+              test: /[\\/]node_modules[\\/](@fortawesome\/react-fontawesome|@fortawesome\/fontawesome-svg-core)[\\/]/,
+            },
+          ],
+        },
+      },
+    },
+  },
+  base: "/",
+  server: { port: 5173 },
+  preview: { port: 5173 },
 });
